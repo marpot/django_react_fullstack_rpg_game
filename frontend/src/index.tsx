1ac0de49113242d3b886210d0f38aab238f489e9
Marcin Potoczny <marcin.potoczny@protonmail.com>
2026-05-19 11:42:50 +0200

    chore(frontend): finalize SCSS architecture foundation with modern Sass modules

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
+import './styles/main.scss';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
